1392b8824f93f6cceb1853b9068513506d65da09
Alex Skrenchuk <alexskr@stanford.edu>
2025-11-07 08:57:54 -0800

    chore(style) fix RuboCop offences

--- test/controllers/test_ontology_submissions_controller.rb
+++ test/controllers/test_ontology_submissions_controller.rb
@@ -1,6 +1,5 @@
-require_relative '../test_case'
+require_relative "../test_case"
 class TestOntologySubmissionsController < TestCase
-
   def after_all
     delete_ontologies_and_submissions
   end
@@ -8,21 +7,21 @@ class TestOntologySubmissionsController < TestCase
   def setup
     delete_ontologies_and_submissions
 
-    @suffix   = SecureRandom.hex(4)
-    @acronym  = "TST#{@suffix}".upcase
-    @name     = "Test Ontology #{@acronym}"
+    @suffix = SecureRandom.hex(4)
+    @acronym = "TST#{@suffix}".upcase
+    @name = "Test Ontology #{@acronym}"
     @test_user, _format, _contact = LinkedData::SampleData::Ontology.ontology_objects
 
     Ontology.new(acronym: @acronym, name: @name, administeredBy: [@test_user]).save
 
-    test_file = File.expand_path("../../data/ontology_files/BRO_v3.1.owl", __FILE__)
+    test_file = File.expand_path("../data/ontology_files/BRO_v3.1.owl", __dir__)
     @file_params = {
       name: @name,
       hasOntologyLanguage: "OWL",
       administeredBy: @test_user,
-      "file" => Rack::Test::UploadedFile.new(test_file, ""),
+      file: Rack::Test::UploadedFile.new(test_file, ""),
       released: Time.now.utc.iso8601,
-      contact: [{ name: "test_name", email: "test3@example.org" }],
+      contact: [{name: "test_name", email: "test3@example.org"}],
       uri: "https://test.com/test",
       status: "production",
       description: "ontology description"
@@ -35,7 +34,7 @@ class TestOntologySubmissionsController < TestCase
     get "/ontologies/#{ontology}/submissions"
     assert last_response.ok?
 
-    submissions_goo = OntologySubmission.where(ontology: { acronym: ontology}).to_a
+    submissions_goo = OntologySubmission.where(ontology: {acronym: ontology}).to_a
 
     submissions = MultiJson.load(last_response.body)
     assert_equal submissions.length, submissions_goo.length
@@ -55,7 +54,7 @@ class TestOntologySubmissionsController < TestCase
     ont = MultiJson.load(last_response.body)
     assert_equal @acronym, ont["acronym"]
     # Cleanup
-    delete "/ontologies/#{@acronym}/submissions/#{sub['submissionId']}"
+    delete "/ontologies/#{@acronym}/submissions/#{sub["submissionId"]}"
     assert_equal(204, last_response.status, get_errors(last_response))
   end
 
@@ -64,17 +63,18 @@ class TestOntologySubmissionsController < TestCase
     assert_equal(201, last_response.status, get_errors(last_response))
     # Cleanup
     sub = MultiJson.load(last_response.body)
-    delete "/ontologies/#{@acronym}/submissions/#{sub['submissionId']}"
+    delete "/ontologies/#{@acronym}/submissions/#{sub["submissionId"]}"
     assert_equal(204, last_response.status, get_errors(last_response))
   end
 
   def test_patch_ontology_submission
     num_onts_created, created_ont_acronyms = create_ontologies_and_submissions(ont_count: 1)
-    ont = Ontology.find(created_ont_acronyms.first).include(submissions: [:submissionId, ontology: :acronym]).first
+    ont = Ontology.find(created_ont_acronyms.first).include(submissions: [:submissionId, {ontology: :acronym}]).first
     assert_operator ont.submissions.length, :>, 0
     submission = ont.submissions[0]
     new_values = {description: "Testing new description changes"}
-    patch "/ontologies/#{submission.ontology.acronym}/submissions/#{submission.submissionId}", MultiJson.dump(new_values), "CONTENT_TYPE" => "application/json"
+    patch "/ontologies/#{submission.ontology.acronym}/submissions/#{submission.submissionId}",
+      MultiJson.dump(new_values), "CONTENT_TYPE" => "application/json"
     assert_equal(204, last_response.status, get_errors(last_response))
     get "/ontologies/#{submission.ontology.acronym}/submissions/#{submission.submissionId}"
     submission = MultiJson.load(last_response.body)
@@ -84,7 +84,7 @@ class TestOntologySubmissionsController < TestCase
   def test_patch_submission_ignores_system_controlled_attributes
     _, acronyms = create_ontologies_and_submissions(ont_count: 1)
     acronym = acronyms.first
-    ontology = Ontology.find(acronym).include(submissions: [:submissionId, ontology: :acronym]).first
+    ontology = Ontology.find(acronym).include(submissions: [:submissionId, {ontology: :acronym}]).first
     assert_operator ontology.submissions.length, :>, 0
     submission = ontology.submissions.first
 
@@ -95,8 +95,8 @@ class TestOntologySubmissionsController < TestCase
     }
 
     patch "/ontologies/#{acronym}/submissions/#{submission.submissionId}",
-          MultiJson.dump(patch_payload),
-          "CONTENT_TYPE" => "application/json"
+      MultiJson.dump(patch_payload),
+      "CONTENT_TYPE" => "application/json"
     assert_equal(204, last_response.status, get_errors(last_response))
 
     get "/ontologies/#{acronym}/submissions/#{submission.submissionId}"
@@ -112,9 +112,10 @@ class TestOntologySubmissionsController < TestCase
   end
 
   def test_delete_ontology_submission
-    num_onts_created, created_ont_acronyms = create_ontologies_and_submissions(ont_count: 1, random_submission_count: false, submission_count: 5)
+    num_onts_created, created_ont_acronyms = create_ontologies_and_submissions(ont_count: 1,
+      random_submission_count: false, submission_count: 5)
     acronym = created_ont_acronyms.first
-    submission_to_delete = (1..5).to_a.shuffle.first
+    submission_to_delete = (1..5).to_a.sample
     delete "/ontologies/#{acronym}/submissions/#{submission_to_delete}"
     assert_equal(204, last_response.status, get_errors(last_response))
 
@@ -137,7 +138,7 @@ class TestOntologySubmissionsController < TestCase
     assert_equal submission_count - delete_ids.size, keep_ids.size, "Unexpected keep_ids size"
 
     # Kick off the long-running bulk delete via the collection DELETE endpoint
-    payload = MultiJson.dump({ ontology_submission_ids: delete_ids })
+    payload = MultiJson.dump({ontology_submission_ids: delete_ids})
     delete "/ontologies/#{acronym}/submissions", payload, "CONTENT_TYPE" => "application/json"
 
     assert_equal(202, last_response.status, get_errors(last_response))
@@ -164,9 +165,9 @@ class TestOntologySubmissionsController < TestCase
       sleep 0.1
     end
 
-     # Validate result payload
+    # Validate result payload
     if status_payload["errors"]
-      flunk "Bulk delete returned errors: #{status_payload['errors'].inspect}"
+      flunk "Bulk delete returned errors: #{status_payload["errors"].inspect}"
     else
       returned_deleted = Array(status_payload["deleted_ids"]).map(&:to_i).sort
       assert_equal(delete_ids, returned_deleted, "Deleted IDs mismatch")
@@ -177,7 +178,8 @@ class TestOntologySubmissionsController < TestCase
     # Deleted ones should be gone
     delete_ids.each do |sid|
       get "/ontologies/#{acronym}/submissions/#{sid}"
-      assert_equal(404, last_response.status, "Submission #{sid} should be gone, but GET returned #{last_response.status} for #{sid}")
+      assert_equal(404, last_response.status,
+        "Submission #{sid} should be gone, but GET returned #{last_response.status} for #{sid}")
     end
 
     # Kept ones should still be present
@@ -188,9 +190,10 @@ class TestOntologySubmissionsController < TestCase
   end
 
   def test_download_submission
-    num_onts_created, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: false)
-    assert_equal(1, num_onts_created, msg="Failed to create 1 ontology?")
-    assert_equal(1, onts.length, msg="Failed to create 1 ontology?")
+    num_onts_created, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1,
+      process_submission: false)
+    assert_equal(1, num_onts_created, "Failed to create 1 ontology?")
+    assert_equal(1, onts.length, "Failed to create 1 ontology?")
     ont = onts.first
     ont.bring(:submissions, :acronym)
     assert_instance_of(Ontology, ont, "ont is not a #{Ontology.class}")
@@ -202,25 +205,27 @@ class TestOntologySubmissionsController < TestCase
     LinkedData::OntologiesAPI.settings.restrict_download = []
     # Download the specific submission
     get "/ontologies/#{ont.acronym}/submissions/#{sub.submissionId}/download"
-    assert_equal(200, last_response.status, 'failed download for specific submission : ' + get_errors(last_response))
+    assert_equal(200, last_response.status, "failed download for specific submission : " + get_errors(last_response))
     # Add restriction on download
     acronym = created_ont_acronyms.first
     LinkedData::OntologiesAPI.settings.restrict_download = [acronym]
     # Try download
     get "/ontologies/#{ont.acronym}/submissions/#{sub.submissionId}/download"
     # download should fail with a 403 status
-    assert_equal(403, last_response.status, 'failed to restrict download for ontology : ' + get_errors(last_response))
+    assert_equal(403, last_response.status, "failed to restrict download for ontology : " + get_errors(last_response))
     # Clear restrictions on downloads
     LinkedData::OntologiesAPI.settings.restrict_download = []
     # see also test_ontologies_controller::test_download_ontology
 
     # Test downloads of nonexistent ontology
     get "/ontologies/BOGUS66/submissions/55/download"
-    assert_equal(422, last_response.status, "failed to handle downloads of nonexistent ontology" + get_errors(last_response))
+    assert_equal(422, last_response.status,
+      "failed to handle downloads of nonexistent ontology" + get_errors(last_response))
   end
 
   def test_download_ontology_submission_rdf
-    count, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: true)
+    count, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1,
+      process_submission: true)
     acronym = created_ont_acronyms.first
     ont = onts.first
     sub = ont.submissions.first
@@ -234,7 +239,8 @@ class TestOntologySubmissionsController < TestCase
   end
 
   def test_download_acl_only
-    count, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: false)
+    count, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1,
+      process_submission: false)
     acronym = created_ont_acronyms.first
     ont = onts.first.bring_remaining
     ont.bring(:submissions)
@@ -282,7 +288,8 @@ class TestOntologySubmissionsController < TestCase
 
   def test_submissions_default_includes
     ontology_count = 5
-    num_onts_created, created_ont_acronyms, ontologies = create_ontologies_and_submissions(ont_count: ontology_count, submission_count: 1, submissions_to_process: [])
+    num_onts_created, created_ont_acronyms, ontologies = create_ontologies_and_submissions(ont_count: ontology_count,
+      submission_count: 1, submissions_to_process: [])
 
     submission_default_attributes = LinkedData::Models::OntologySubmission.hypermedia_settings[:serialize_default].map(&:to_s)
 
@@ -307,10 +314,11 @@ class TestOntologySubmissionsController < TestCase
 
   def test_submissions_all_includes
     ontology_count = 5
-    num_onts_created, created_ont_acronyms, ontologies = create_ontologies_and_submissions(ont_count: ontology_count, submission_count: 1, submissions_to_process: [])
+    num_onts_created, created_ont_acronyms, ontologies = create_ontologies_and_submissions(ont_count: ontology_count,
+      submission_count: 1, submissions_to_process: [])
     def submission_all_attributes
       attrs = OntologySubmission.goo_attrs_to_load([:all])
-      embed_attrs = attrs.select { |x| x.is_a?(Hash) }.first
+      embed_attrs = attrs.find { |x| x.is_a?(Hash) }
 
       attrs.delete_if { |x| x.is_a?(Hash) }.map(&:to_s) + embed_attrs.keys.map(&:to_s)
     end
@@ -353,8 +361,9 @@ class TestOntologySubmissionsController < TestCase
 
   def test_submissions_custom_includes
     ontology_count = 5
-    num_onts_created, created_ont_acronyms, ontologies = create_ontologies_and_submissions(ont_count: ontology_count, submission_count: 1, submissions_to_process: [])
-    include = 'ontology,contact,submissionId'
+    num_onts_created, created_ont_acronyms, ontologies = create_ontologies_and_submissions(ont_count: ontology_count,
+      submission_count: 1, submissions_to_process: [])
+    include = "ontology,contact,submissionId"
 
     get("/submissions?include=#{include}&display_links=false&display_context=false")
 
@@ -362,7 +371,7 @@ class TestOntologySubmissionsController < TestCase
     submissions = MultiJson.load(last_response.body)
     assert_equal ontology_count, submissions.size
     submissions.each do |sub|
-      assert_equal(include.split(','), submission_keys(sub))
+      assert_equal(include.split(","), submission_keys(sub))
       assert_submission_contact_structure(sub)
     end
 
@@ -372,33 +381,33 @@ class TestOntologySubmissionsController < TestCase
     submissions = MultiJson.load(last_response.body)
     assert_equal(1, submissions.size)
     submissions.each do |sub|
-      assert_equal(include.split(','), submission_keys(sub))
+      assert_equal(include.split(","), submission_keys(sub))
       assert_submission_contact_structure(sub)
     end
 
     get("/ontologies/#{created_ont_acronyms.first}/latest_submission?include=#{include}&display_links=false&display_context=false")
     assert(last_response.ok?)
     sub = MultiJson.load(last_response.body)
-    assert_equal(include.split(','), submission_keys(sub))
+    assert_equal(include.split(","), submission_keys(sub))
     assert_submission_contact_structure(sub)
 
     get("/ontologies/#{created_ont_acronyms.first}/submissions/1?include=#{include}&display_links=false&display_context=false")
     assert(last_response.ok?)
     sub = MultiJson.load(last_response.body)
-    assert_equal(include.split(','), submission_keys(sub))
+    assert_equal(include.split(","), submission_keys(sub))
     assert_submission_contact_structure(sub)
   end
 
   private
+
   def submission_keys(sub)
     sub.to_hash.keys - %w[@id @type id]
   end
 
   def assert_submission_contact_structure(sub)
     assert sub["contact"], "Contact should be present"
-    if sub["contact"].first
-      assert_equal(%w[name email id].sort, sub["contact"].first.keys.sort)
-    end
-  end
+    return unless sub["contact"].first
 
+    assert_equal(%w[name email id].sort, sub["contact"].first.keys.sort)
+  end
 end
